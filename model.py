42f3bdb319c430eda1d4a7bcdb31c0f969c6ee9b
Ahsan Khan <ahsan.al.zaki@gmail.com>
2019-04-08 05:06:01 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,23 +1,30 @@
 import tensorflow as tf
 
+# Callback function to set callback when desired accuracy is reached
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('acc')>0.99):
       print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
 
+#Loading the dataset
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+#Normalizing the data
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 callbacks = myCallback()
 
+#Building the model
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(512, activation=tf.nn.relu),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+
+#Optimizing loss function and setting metrics
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
